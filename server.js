a8d557f35dc6519b0b8e1feee789a56d40f3a0dc
aishabraud92 <aishabraud@gmail.com>
2018-01-12 11:36:30 -0800

    auth add

---
+++ server.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var flash = require('connect-flash');
+var bodyParser = require('body-parser');
+var request = require('request');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var passport = require('./config/passportConfig');
+var session = require('express-session');
+var app = express();
+
+
+const apiKey = 'd92f1c0ae2430c69eb20fefc46916e5e';
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extented: false}));
+app.use(ejsLayouts);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
+});
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.get('/profile', isLoggedIn, function(req, res){
+  res.render('profile');
+});
+
+app.use('/auth', require('./controllers/auth'));
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+});
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.get('/profile', isLoggedIn, function(req,res){
+
+});
+
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+});
